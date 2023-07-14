237596888741f7f23171459eb6d0c60db7206d7a
hammadx02 <hammadaali02@gmail.com>
2023-07-14 23:51:44 +0500

    initial commit

--- lib/constents/constents.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color colorGrey = Color(0xffA5A5A5);
-const Color colorBlack = Color(0xff303136);
-const Color colorDarkBlue = Color(0xff005DB2);
-const Color colorBlue = Color(0xff1991FF);
-
-
-
